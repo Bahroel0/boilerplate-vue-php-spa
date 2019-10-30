5c0b3a8cd823c293293528b8c16d24cf543d20bc
Bahroel0 <bahrul123amaruddin@gmail.com>
2019-10-30 14:37:31 +0700

    add env.example

---
+++ env.example.php
@@ -0,0 +1,13 @@
+<?php
+  $variables = [
+      'APP_KEY' => '',
+      'DB_HOST' => 'localhost',
+      'DB_USERNAME' => '',
+      'DB_PASSWORD' => '',
+      'DB_NAME' => '',
+      'DB_PORT' => '3306',
+  ];
+  foreach ($variables as $key => $value) {
+      putenv("$key=$value");
+  }
+?>
